29b4b371fee1874ad85078e17a0d6988cc988b79
mizunosuke <m.h.0656.longjump@icloud.com>
2023-01-04 10:38:11 +0900

    first commit

---
+++ resources/js/Components/Header.jsx
@@ -0,0 +1,28 @@
+import { Link } from "@inertiajs/inertia-react"
+
+export const Header = () => {
+    return (
+        <div>
+            <div className="fixed top-0 right-0 px-6 py-4 sm:block">
+                    {props.auth.user ? (
+                        <Link href={route('dashboard')} className="text-sm text-gray-700 dark:text-gray-500 underline">
+                            Dashboard
+                        </Link>
+                    ) : (
+                        <>
+                            <Link href={route('login')} className="text-sm text-gray-700 dark:text-gray-500 underline">
+                                Log in
+                            </Link>
+
+                            <Link
+                                href={route('register')}
+                                className="ml-4 text-sm text-gray-700 dark:text-gray-500 underline"
+                            >
+                                Register
+                            </Link>
+                        </>
+                    )}
+                </div>
+        </div>
+    )
+}
---
+++ resources/js/Pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import { Header } from "@/Components/Header";
+
+export const Home = () => {
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
+}
