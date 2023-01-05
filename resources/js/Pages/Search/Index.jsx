ffe011d8e53f53776a3853695a48496d5b260ba8
mizunosuke <m.h.0656.longjump@icloud.com>
2023-01-05 16:55:31 +0900

    user情報のコメントアウト外した

--- resources/js/Pages/Search/Index.jsx
+++ resources/js/Pages/Search/Index.jsx
@@ -16,6 +16,10 @@ export default function PortSearch (props) {
                 <Head title="Search" />
 
             </AuthenticatedLayout>
+
+            <div>
+                
             </div>
-        )
-    }
+        </div>
+    )
+}
